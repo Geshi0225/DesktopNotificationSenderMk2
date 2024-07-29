ccc7ae22f3f657c4d29d41af6ec781550e135a49
riyousya <riyousya@DESKTOP-4PI3E2I>
2024-07-29 15:17:21 +0900

    プロジェクト ファイルを追加します。

---
+++ DesktopNotificationSenderMk2/Program.cs
@@ -0,0 +1,21 @@
+using Windows.Foundation.Metadata;
+using Windows.UI.Notifications;
+using Windows.UI.Notifications.Management;
+
+namespace DesktopNotificationSenderMk2
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
