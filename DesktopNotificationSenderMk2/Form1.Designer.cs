4a3515d1141a2c136cdf4c4238aff34d7132fc25
riyousya <riyousya@DESKTOP-4PI3E2I>
2024-07-29 15:25:48 +0900

    各コントロールを追加、一部ラベルも変更

--- DesktopNotificationSenderMk2/Form1.Designer.cs
+++ DesktopNotificationSenderMk2/Form1.Designer.cs
@@ -28,18 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            menuStrip1 = new MenuStrip();
+            設定ToolStripMenuItem = new ToolStripMenuItem();
+            終了ToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 81);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(507, 289);
+            listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 31);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(93, 31);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 15);
+            label1.TabIndex = 3;
+            label1.Text = "受信した通知";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { 設定ToolStripMenuItem, 終了ToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(532, 24);
+            menuStrip1.TabIndex = 4;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // 設定ToolStripMenuItem
+            // 
+            設定ToolStripMenuItem.Name = "設定ToolStripMenuItem";
+            設定ToolStripMenuItem.Size = new Size(43, 20);
+            設定ToolStripMenuItem.Text = "設定";
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            終了ToolStripMenuItem.Alignment = ToolStripItemAlignment.Right;
+            終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            終了ToolStripMenuItem.Size = new Size(43, 20);
+            終了ToolStripMenuItem.Text = "終了";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(427, 305);
+            ClientSize = new Size(532, 383);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem 設定ToolStripMenuItem;
+        private ToolStripMenuItem 終了ToolStripMenuItem;
     }
 }
